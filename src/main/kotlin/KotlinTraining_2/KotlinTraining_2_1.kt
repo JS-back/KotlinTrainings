3cdbc8bc1ac1f0538229915d1534e42b9915149f
JS-back <artast@mail.ru>
2025-02-10 21:28:04 +0400

    KT-2-1-re-change

--- src/main/kotlin/KotlinTraining_2/KotlinTraining_2_1.kt
+++ src/main/kotlin/KotlinTraining_2/KotlinTraining_2_1.kt
@@ -1,11 +1,15 @@
 package org.example.KotlinTraining_2
 
 fun main() {
-    val scores = listOf(3, 4, 3, 5)
-    val averageScore = scores.average()
-    println("Средний балл: $averageScore")
+    val numberOfStudents: Int = 4
+    val score1: Int = 3
+    val score2: Int = 4
+    val score3: Int = 3
+    val score4: Int = 5
+    val totalScore: Int = score1 + score2 + score3 + score4 // Сумма всех баллов
+    val averageScore: Double = totalScore.toDouble() / numberOfStudents // Средний балл
 
-    fun calculateAverage(scores: List<Int>): Double {
-        val total = scores.sum()
-        return total.toDouble() / scores.size
-    }
+    println("Общее количество учеников: $numberOfStudents")
+    println("Сумма баллов: $totalScore")
+    println("Средний балл: $averageScore")
+}
