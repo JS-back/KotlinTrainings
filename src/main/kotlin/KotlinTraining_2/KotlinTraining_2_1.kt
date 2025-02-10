b3f4f420240f340408ad9b8339c9d67af16c17ee
JS-back <artast@mail.ru>
2025-02-10 20:44:14 +0400

    KT-2-1-refactor smth

--- src/main/kotlin/KotlinTraining_2/KotlinTraining_2_1.kt
+++ src/main/kotlin/KotlinTraining_2/KotlinTraining_2_1.kt
@@ -4,4 +4,8 @@ fun main() {
     val scores = listOf(3, 4, 3, 5)
     val averageScore = scores.average()
     println("Средний балл: $averageScore")
-}
+
+    fun calculateAverage(scores: List<Int>): Double {
+        val total = scores.sum()
+        return total.toDouble() / scores.size
+    }
